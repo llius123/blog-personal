a02077788e9baf88a9c1a0e977270c17943d97ee
Jesus <llius619@gmail.com>
2022-04-04 14:41:24 +0200

    Revert "Fix master?"
    This reverts commit 847fba73e4237a0a17b666647068e6258bb68173.

--- pages/biblioteca/[id].tsx
+++ pages/biblioteca/[id].tsx
@@ -1,11 +1,9 @@
 import { GetStaticPaths } from "next";
-import { NextRouter, useRouter } from "next/router";
-import UIBodyContainer from "../../components/display-components/UIBodyContainer";
+import { useRouter } from "next/router";
 import UIHeader from "../../components/header/UIHeader";
-import { UILoading } from "../../components/loading/UILoading";
-import { UIPost } from "../../components/post/UIPost";
+import { UIPostWrapper } from "../../components/post/UIPostWrapper";
+import { GetPostMetadata } from "../../lib/post/GetPostMetadata";
 import { PostInterface } from "../../lib/post/PostInterface";
-import { PostRepo } from "../../lib/post/PostRepo";
 
 export default function Post(props: { postData: PostInterface }): JSX.Element {
   const router = useRouter();
@@ -13,24 +11,11 @@ export default function Post(props: { postData: PostInterface }): JSX.Element {
   return (
     <>
       <UIHeader />
-      <PostData router={router} postData={props.postData} />
+      <UIPostWrapper router={router} postData={props.postData} />
     </>
   );
 }
 
-function PostData({
-  router,
-  postData,
-}: {
-  router: NextRouter;
-  postData: PostInterface;
-}): JSX.Element {
-  return (
-    <UIBodyContainer>
-      {router.isFallback ? <UILoading /> : <UIPost postData={postData} />}
-    </UIBodyContainer>
-  );
-}
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
   return {
     paths: [], //indicates that no page needs be created at build time
@@ -38,14 +23,7 @@ export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
   };
 };
 
-export async function getStaticProps({ params }) {
-  const post = new PostRepo('biblioteca')
-  const postData = await post.getPostMetadata(params.id);
 
-  // const postData = { id: "123" };
-  return {
-    props: {
-      postData,
-    },
-  };
+export async function getStaticProps({ params }) { 
+  return await new GetPostMetadata('biblioteca', params.id).run()
 }
